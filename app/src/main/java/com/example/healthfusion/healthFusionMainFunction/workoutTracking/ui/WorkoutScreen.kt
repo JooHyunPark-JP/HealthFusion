e0696bae28c687cf129912e1189ed66eb1d15bf2
JooHyun <pjh0629@gmail.com>
2024-08-29 12:53:08 -0400

    Created section for workout goals in workoutscreen.kt

--- app/src/main/java/com/example/healthfusion/healthFusionMainFunction/workoutTracking/ui/WorkoutScreen.kt
+++ app/src/main/java/com/example/healthfusion/healthFusionMainFunction/workoutTracking/ui/WorkoutScreen.kt
@@ -1,15 +1,18 @@
 package com.example.healthfusion.healthFusionMainFunction.workoutTracking.ui
 
-import android.util.Log
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -21,6 +24,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.healthfusion.healthFusionMainFunction.workoutTracking.data.WorkoutType
@@ -42,6 +47,37 @@ fun WorkoutScreen(viewModel: WorkoutViewModel, modifier: Modifier = Modifier) {
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(text = "Workout Page", fontSize = 24.sp)
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp)
+                .padding(16.dp)
+                .border(1.dp, Color.Gray)
+                .clickable {
+                    // Move to workout goal page logic here
+                }
+                .padding(16.dp)
+        ) {
+            Text(text = "Today's Workout Goals", fontSize = 20.sp, fontWeight = FontWeight.Bold)
+            Spacer(modifier = Modifier.height(8.dp))
+            //will change to progress bar for this text
+            Text(text = "You can set your daily workout goals here.")
+            Spacer(modifier = Modifier.height(8.dp))
+            Button(onClick = {
+                //Move to workout goal page logic here
+            }) {
+                Text("Set Workout Goal")
+            }
+        }
+
+        // 구분선 추가
+        HorizontalDivider(
+            color = Color.Gray,
+            thickness = 1.dp,
+            modifier = Modifier.padding(vertical = 16.dp)
+        )
+
 
         TextField(
             value = name,
